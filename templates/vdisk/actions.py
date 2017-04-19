f366723ce33f6e41739a38b06330c8a03ae6f553
Mohamed Elawadi <mohamed@elawadi.net>
2017-04-19 15:38:57 +0200

    volume: create from a volume template

--- templates/vdisk/actions.py
+++ templates/vdisk/actions.py
@@ -4,7 +4,68 @@ from JumpScale import j
 def install(job):
     service = job.service
     service.model.data.status = 'halted'
+    if service.model.data.templateVolume:
+        services = service.aysrepo.servicesFind(role='grid_config')
+        if len(services) <= 0:
+            raise j.exceptions.NotFound("not grid_config service installed. {} can't get the grid API URL.".format(service))
 
+        grid_addr = services[0].model.data.apiURL
+
+        args = job.model.args
+        target_node = service.aysrepo.serviceGet('node', args['node'])
+
+        volume_container = create_nbdserver_container(service, target_node)
+        try:
+            node_client = j.clients.g8core.get(host=target_node.model.data.redisAddr,
+                                               port=target_node.model.data.redisPort,
+                                               password=target_node.model.data.redisPassword)
+            container_client = node_client.container.client(volume_container.model.data.id)
+            CMD = 'copyvolume -t api {src_name} {dst_name} -a {grid_addr} {src_storagecluster} {dst_storagecluster}'
+            cmd = CMD.format(src_name=service.name, dst_name=service.model.data.templateVolume.name, grid_addr=grid_addr,
+                             src_storagecluster=service.model.data.storageCluster.name, dst_storagecluster=service.model.data.templateVolume.name)
+            container_client.bash(cmd)
+
+        finally:
+            destroy_nbdserver_container(service, target_node)
+
+
+def create_nbdserver_container(service, parent):
+    """
+    first check if the volumes container for this vm exists.
+    if not it creates it.
+    return the container service
+    """
+    container_name = 'volumes_{}_{}'.format(service.name, parent.name)
+    try:
+        container = service.aysrepo.serviceGet(role='container', instance=container_name)
+    except j.exceptions.NotFound:
+        container = None
+
+    if container is None:
+        container_actor = service.aysrepo.actorGet('container')
+        args = {
+            'node': parent.name,
+            'flist': 'https://hub.gig.tech/gig-official-apps/blockstor-master.flist',
+            'hostNetworking': True,
+        }
+        container = container_actor.serviceCreate(instance=container_name, args=args)
+
+    return container
+
+
+def destroy_nbdserver_container(service, parent):
+    """
+    first check if the volumes container for this vm exists.
+    if not it creates it.
+    return the container service
+    """
+    container_name = 'volumes_{}_{}'.format(service.name, parent.name)
+    try:
+        container = service.aysrepo.serviceGet(role='container', instance=container_name)
+    except j.exceptions.NotFound:
+        container = None
+    else:
+        container.delete()
 
 def start(job):
     service = job.service
