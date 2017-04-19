cfeff2807839c83fa8bafde804e97371162a1e8b
Abdulrahman tarek khalifa <Abdulrahman.t.khalifa@gmail.com>
2017-04-19 17:44:24 +0200

    renamed the template for volumes to vdisks

--- buildscripts/builder-blockstor.sh
+++ buildscripts/builder-blockstor.sh
@@ -20,7 +20,7 @@ export GOPATH=/gopath
 BLOCKSTOR=$GOPATH/src/github.com/g8os/blockstor
 
 go get -v -d github.com/g8os/blockstor/nbdserver
-go get -v -d github.com/g8os/blockstor/cmd/copyvdisk
+go get -v -d github.com/g8os/blockstor/cmd/copyvolume
 
 cd $BLOCKSTOR
 
@@ -30,8 +30,8 @@ git checkout "${branch}" || true
 cd $BLOCKSTOR/nbdserver
 CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' .
 
-cd $BLOCKSTOR/cmd/copyvdisk
+cd $BLOCKSTOR/cmd/copyvolume
 CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' .
 
 mkdir -p /tmp/archives/
-tar -czf "/tmp/archives/blockstor-${branch}.tar.gz" -C $BLOCKSTOR/nbdserver nbdserver -C $BLOCKSTOR/cmd/copyvdisk copyvdisk
+tar -czf "/tmp/archives/blockstor-${branch}.tar.gz" -C $BLOCKSTOR/nbdserver nbdserver -C $BLOCKSTOR/cmd/copyvolume copyvolume
