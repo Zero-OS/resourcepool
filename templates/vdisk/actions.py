d8c5fe78fe797601f3b98ef5ec5786103f575959
Jo De Boeck <deboeck.jo@gmail.com>
2017-04-23 20:48:37 +0200

    Update blockstor builder and get vdisk from template to work
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- templates/vdisk/actions.py
+++ templates/vdisk/actions.py
@@ -2,70 +2,76 @@ from JumpScale import j
 
 
 def install(job):
+    import random
     service = job.service
     service.model.data.status = 'halted'
-    if service.model.data.templateVolume:
+    if service.model.data.templateVdisk:
         services = service.aysrepo.servicesFind(role='grid_config')
         if len(services) <= 0:
             raise j.exceptions.NotFound("not grid_config service installed. {} can't get the grid API URL.".format(service))
 
         grid_addr = services[0].model.data.apiURL
+        storagecluster = service.aysrepo.serviceGet(role='storage_cluster', instance=service.model.data.storageCluster)
+        target_node = random.choice(storagecluster.producers['node'])
 
-        args = job.model.args
-        target_node = service.aysrepo.serviceGet('node', args['node'])
-
-        volume_container = create_nbdserver_container(service, target_node)
+        volume_container = create_from_template_container(service, target_node)
         try:
             node_client = j.clients.g8core.get(host=target_node.model.data.redisAddr,
                                                port=target_node.model.data.redisPort,
                                                password=target_node.model.data.redisPassword)
             container_client = node_client.container.client(volume_container.model.data.id)
-            CMD = 'copyvolume -t api {src_name} {dst_name} -a {grid_addr} {src_storagecluster} {dst_storagecluster}'
-            cmd = CMD.format(src_name=service.name, dst_name=service.model.data.templateVolume.name, grid_addr=grid_addr,
-                             src_storagecluster=service.model.data.storageCluster.name, dst_storagecluster=service.model.data.templateVolume.name)
-            container_client.bash(cmd)
+            CMD = '/copyvdisk -t api -sourcesc {src_storagecluster} -targetsc {dst_storagecluster} {src_name} {dst_name} {grid_addr}'
+            cmd = CMD.format(dst_name=service.name, src_name=service.model.data.templateVdisk, grid_addr=grid_addr,
+                             src_storagecluster=service.model.data.storageCluster, dst_storagecluster=service.model.data.storageCluster)
+            print(cmd)
+            result = container_client.system(cmd).get()
+            if result.state != 'SUCCESS':
+                j.exceptions.RuntimeError("Failed to copyvdisk {} {}".format(result.stdout, result.stderr))
 
         finally:
-            destroy_nbdserver_container(service, target_node)
+            destroy_from_template_container(service, target_node)
 
 
-def create_nbdserver_container(service, parent):
+def create_from_template_container(service, parent):
     """
-    first check if the volumes container for this vm exists.
     if not it creates it.
     return the container service
     """
-    container_name = 'volumes_{}_{}'.format(service.name, parent.name)
+    container_name = 'vdisk_{}_{}'.format(service.name, parent.name)
     try:
         container = service.aysrepo.serviceGet(role='container', instance=container_name)
     except j.exceptions.NotFound:
         container = None
+    if container:
+        container.delete()
 
-    if container is None:
-        container_actor = service.aysrepo.actorGet('container')
-        args = {
-            'node': parent.name,
-            'flist': 'https://hub.gig.tech/gig-official-apps/blockstor-master.flist',
-            'hostNetworking': True,
-        }
-        container = container_actor.serviceCreate(instance=container_name, args=args)
+    container_actor = service.aysrepo.actorGet('container')
+    args = {
+        'node': parent.name,
+        'flist': 'https://hub.gig.tech/gig-official-apps/blockstor-master.flist',
+        'hostNetworking': True,
+    }
+    container = container_actor.serviceCreate(instance=container_name, args=args)
+    j.tools.async.wrappers.sync(container.executeAction('install'))
 
     return container
 
 
-def destroy_nbdserver_container(service, parent):
+def destroy_from_template_container(service, parent):
     """
     first check if the volumes container for this vm exists.
     if not it creates it.
     return the container service
     """
-    container_name = 'volumes_{}_{}'.format(service.name, parent.name)
+    container_name = 'vdisk_{}_{}'.format(service.name, parent.name)
     try:
         container = service.aysrepo.serviceGet(role='container', instance=container_name)
     except j.exceptions.NotFound:
         container = None
     else:
-        container.delete()
+        j.tools.async.wrappers.sync(container.executeAction('stop'))
+        j.tools.async.wrappers.sync(container.delete())
+
 
 def start(job):
     service = job.service
