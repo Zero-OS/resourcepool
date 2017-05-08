44f72b83636c7e3e9d675283b5ea137c56df032a
Abdulrahman tarek khalifa <Abdulrahman.t.khalifa@gmail.com>
2017-05-08 13:32:18 +0200

    bug fix concerning the default value of nic id
    if type is default , id can be empty else
        id cannot be empty.

--- api/node/ContainerNIC.go
+++ api/node/ContainerNIC.go
@@ -1,8 +1,10 @@
 package node
 
 import (
-	"gopkg.in/validator.v2"
+	"errors"
+
 	"github.com/g8os/resourcepool/api/validators"
+	"gopkg.in/validator.v2"
 )
 
 type ContainerNICConfig struct {
@@ -15,19 +17,33 @@ type ContainerNICConfig struct {
 type ContainerNIC struct {
 	Config ContainerNICConfig   `json:"config,omitempty" yaml:"config,omitempty"`
 	Hwaddr string               `json:"hwaddr,omitempty" yaml:"hwaddr,omitempty" validate:"macaddress=empty"`
-	Id     string               `json:"id" yaml:"id" validate:"nonzero"`
+	Id     string               `json:"id,omitempty" yaml:"id,omitempty"`
 	Type   EnumContainerNICType `json:"type" yaml:"type" validate:"nonzero"`
+}
+
+func (s ContainerNIC) validateID() error {
+	if s.Type != "default" {
+		if s.Id == "" {
+			return errors.New("validation error on Id of NIC: cannot be empty")
+		}
+		return nil
+	}
+	return nil
 }
 
 func (s ContainerNIC) Validate() error {
 	typeEnums := map[interface{}]struct{}{
 		EnumContainerNICTypezerotier: struct{}{},
 		EnumContainerNICTypevxlan:    struct{}{},
-		EnumContainerNICTypevlan:  struct{}{},
-		EnumContainerNICTypedefault: struct {}{},
+		EnumContainerNICTypevlan:     struct{}{},
+		EnumContainerNICTypedefault:  struct{}{},
 	}
 
 	if err := validators.ValidateEnum("Type", s.Type, typeEnums); err != nil {
+		return err
+	}
+
+	if err := s.validateID(); err != nil {
 		return err
 	}
 
