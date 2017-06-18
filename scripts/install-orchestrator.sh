487d429bc9fd9ced62f6bc7b918a56507dfeba88
Abdulrahman <tareka@codescalers.com>
2017-06-18 12:32:41 +0200

    bug fixes in the install script

--- scripts/install-orchestrator.sh
+++ scripts/install-orchestrator.sh
@@ -98,18 +98,20 @@ popd
 echo "[+] Start AtYourService server"
 
 aysinit="/etc/my_init.d/10_ays.sh"
+if [ -z ${ITSYOUONLINEORG} ]; then
 if [ ! -d /optvar/cfg/ ]; then
     mkdir /optvar/cfg/
 fi 
 cat >  /optvar/cfg/jumpscale9.toml << EOL
 [ays]        
 production = true
-                                                  
+                                                
 [ays.oauth] 
 client_secret = "${CLIENTSECRET}" 
-jwt_key = "MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAES5X8XrfKdx9gYayFITc89wad4usrk0n27MjiGYvqalizeSWTHEpnd7oea9IQ8T5oJjMVH5cc0H5tFSKilFFeh//      wngxIy" 
+jwt_key = "MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAES5X8XrfKdx9gYayFITc89wad4usrk0n27MjiGYvqalizeSWTHEpnd7oea9IQ8T5oJjMVH5cc0H5tFSKilFFeh//wngxIyny66+Vq5t5B0V0Ehy01+2ceEon2Y0XDkIKv" 
 organization = "${ITSYOUONLINEORG}"  
 EOL
+fi 
 
 echo '#!/bin/bash -x' > ${aysinit}
 echo 'ays start > /dev/null 2>&1' >> ${aysinit}
