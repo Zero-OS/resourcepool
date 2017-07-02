7461d47b079520d495e5dc860b7abda387cae48d
Abdulrahman <tareka@codescalers.com>
2017-07-02 12:16:21 +0200

    added condition to check if zerotier-one is running
    to avoid error when trying to run twice

--- scripts/install-orchestrator.sh
+++ scripts/install-orchestrator.sh
@@ -71,7 +71,10 @@ mkdir -p /etc/my_init.d > ${logfile} 2>&1
 ztinit="/etc/my_init.d/10_zerotier.sh"
 
 echo '#!/bin/bash -x' > ${ztinit}
-echo 'zerotier-one -d' >> ${ztinit}
+ps cax| grep zerotier-one
+if [ ! $? -eq 0 ]; then 
+    echo 'zerotier-one -d' >> ${ztinit}
+fi
 echo 'while ! zerotier-cli info > /dev/null 2>&1; do sleep 0.1; done' >> ${ztinit}
 echo "[ $ZEROTIERNWID != \"\" ] && zerotier-cli join $ZEROTIERNWID" >> ${ztinit}
 
