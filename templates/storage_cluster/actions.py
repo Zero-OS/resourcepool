7c88105a3805259db89505bd11902c38a118e611
Khaled K. Badr <khaledkbadr@outlook.com>
2017-07-03 15:51:38 +0200

    Fix storage cluster resource conflict
    Signed-off-by: Khaled K. Badr <khaledkbadr@outlook.com>

--- templates/storage_cluster/actions.py
+++ templates/storage_cluster/actions.py
@@ -32,7 +32,6 @@ def get_cluster(job):
 
 def init(job):
     from zeroos.orchestrator.configuration import get_configuration
-    from zeroos.orchestrator.sal.StorageCluster import StorageCluster
     from zeroos.orchestrator.sal.Node import Node
 
     service = job.service
@@ -42,8 +41,7 @@ def init(job):
     nodes = list(nodes)
     nodemap = {node.name: node for node in nodes}
 
-    cluster = StorageCluster(service.name, nodes, service.model.data.diskType)
-    availabledisks = cluster.find_disks()
+    availabledisks = get_availabledisks(job, nodes)
     diskpernode = int(service.model.data.nrServer / len(nodes))
     # validate amount of disks and remove unneeded disks
     if service.model.data.nrServer % len(nodes) != 0:
@@ -103,7 +101,7 @@ def init(job):
     for nodename, disks in availabledisks.items():
         node = nodemap[nodename]
         # making the storagepool
-        baseports = node.freeports(baseport=2000, nrports=len(disks) + 1)
+        baseports = get_baseports(job, node, baseport=2000, nrports=len(disks) + 1)
         for idx, disk in enumerate(disks):
             create_server(node, disk, baseports[idx])
 
@@ -121,6 +119,56 @@ def init(job):
         service.model.data.ardbs[index] = ardb.name
 
     job.service.model.data.status = 'empty'
+
+
+def get_availabledisks(job, nodes):
+    from zeroos.orchestrator.sal.StorageCluster import StorageCluster
+
+    service = job.service
+    used_disks = {}
+    for node in nodes:
+        disks = set()
+        pools = service.aysrepo.servicesFind(role='storagepool', parent='node.zero-os!%s' % node.name)
+        for pool in pools:
+            devices = {device.device for device in pool.model.data.devices}
+            disks.update(devices)
+        used_disks[node.name] = disks
+
+    cluster = StorageCluster(service.name, nodes, service.model.data.diskType)
+    availabledisks = cluster.find_disks()
+    freedisks = {}
+    for node, disks in availabledisks.items():
+        node_disks = []
+        for disk in disks:
+            if disk.devicename not in used_disks[node]:
+                node_disks.append(disk)
+        freedisks[node] = node_disks
+    return freedisks
+
+
+def get_baseports(job, node, baseport, nrports):
+    service = job.service
+    tcps = service.aysrepo.servicesFind(role='tcp', parent='node.zero-os!%s' % node.name)
+
+    usedports = set()
+    for tcp in tcps:
+        usedports.add(tcp.model.data.port)
+
+    freeports = []
+    tcpactor = service.aysrepo.actorGet("tcp")
+    while True:
+        if baseport not in usedports:
+            baseport = node.freeports(baseport=baseport, nrports=1)[0]
+            args = {
+                'node': node.name,
+                'port': baseport,
+            }
+            tcp = 'tcp_{}_{}'.format(node.name, baseport)
+            tcpactor.serviceCreate(instance=tcp, args=args)
+            freeports.append(baseport)
+            if len(freeports) >= nrports:
+                return freeports
+        baseport += 1
 
 
 def install(job):
