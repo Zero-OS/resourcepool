13e52c02680766a91e9781e39156c4623a8bbe25
ahmedelsayed-93m <elsayeda@greenitglobe.com>
2017-07-17 10:38:18 +0200

    use iyo to generate jwt

--- tests/Grid_API_Testing/api_testing/orchestrator_api/orchestrator_base.py
+++ tests/Grid_API_Testing/api_testing/orchestrator_api/orchestrator_base.py
@@ -1,5 +1,6 @@
 from zeroos.orchestrator import client as apiclient
 from testconfig import config
+import requests
 
 api_base_url = config['main']['api_base_url']
 client_id = config['main']['client_id']
@@ -15,7 +16,17 @@ class GridPyclientBase(object):
 
     def get_jwt(self):
         auth = apiclient.oauth2_client_itsyouonline.Oauth2ClientItsyouonline()
-        response = auth.get_access_token(client_id, client_secret, scopes=['user:memberof:%s' % organization], audiences=[])
+        # response = auth.get_access_token(client_id, client_secret, scopes=['user:memberof:%s' % organization], audiences=[])
+        params = {
+            "grant_type": "client_credentials",
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "response_type": "id_token",
+            "scope": "user:memberof:{},offline_access".format(organization),
+            "validity":3600
+        }
+
+        response = requests.post(url='https://itsyou.online/v1/oauth/access_token', params=params)
         if response.status_code == 200:
             return response.content.decode('utf-8')
         else:
