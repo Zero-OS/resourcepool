7fa26cbafcd09a743c30182d54630ed674df1e75
serboctor <boctor.sarah@gmail.com>
2017-07-26 19:56:58 +0300

    Implement ofd healthcheck (#760)
    * Implement ofd healthcheck.
    
    * Add category to ofd healthcheck result.
    
    * Fix ofd messages.
    
    * Refactor healthchecks:
    - Change healthchecks schema to include a list of messages.
    - Refactor all healthchecks to return a list of messages.
    
    * Generate api.html
    
    * Regenrate node capnp

--- pyclient/zeroos/orchestrator/sal/healthchecks/openfiledescriptors.py
+++ pyclient/zeroos/orchestrator/sal/healthchecks/openfiledescriptors.py
@@ -1,17 +1,51 @@
-import json
+import psutil
 
 
-def main():
-    messages = [{
-        'id': 'openfiledescriptors',
-        'name': 'openfiledescriptors',
-        'resource': 'System Load',
-        'status': 'OK',
-        'category': 'System',
-        'message': 'All openfilescriptors are ok'
-    }]
-    print(json.dumps(messages))
+descr = """
+Check open file descriptors for each node process, if it exceeds 90% of the soft limit, it raises a warning,
+if it exceeds 90% of the hard limit, it raises an error.
+"""
+
+
+def action(node):
+    result = {
+        'id': 'OPENFILEDESCRIPTORS',
+        'name': 'Open File Descriptors',
+        'resource': '/nodes/{}'.format(node.name),
+        'category': 'System Load',
+        'messages': list(),
+    }
+
+    for process in node.client.process.list():
+        for rlimit in process['rlimit']:
+            if rlimit['resource'] == psutil.RLIMIT_NOFILE:
+                pid = str(process['pid'])
+                if (0.9 * rlimit['soft']) <= process['ofd'] < (0.9 * rlimit['hard']):
+                    message = {
+                        'id': pid,
+                        'status': 'WARNING',
+                        'text': 'Open file descriptors for process %s exceeded 90%% of the soft limit' % pid,
+                    }
+                    result['messages'].append(message)
+                elif process['ofd'] >= (0.9 * rlimit['hard']):
+                    result['status'] = 'ERROR'
+                    result['message'] = 'Open file descriptors for process %s exceeded 90%% of the hard limit' % pid
+                    message = {
+                        'id': pid,
+                        'status': 'ERROR',
+                        'text': 'Open file descriptors for process %s exceeded 90%% of the hard limit' % pid,
+                    }
+                    result['messages'].append(message)
+                break
+
+    if not result['messages']:
+        result['messages'] = [{
+            'id': '-1',
+            'status': 'OK',
+            'text': 'Open file descriptors for all processes are within limit',
+        }]
+    return [result]
 
 
 if __name__ == '__main__':
-    main()
+    action()
--- templates/node.zero-os/actions.py
+++ templates/node.zero-os/actions.py
@@ -145,43 +145,37 @@ def monitor(job):
     else:
         service.model.data.status = 'halted'
 
-    flist = config.get('healthcheck-flist', 'https://hub.gig.tech/deboeckj/js9container.flist')
-    with node.healthcheck.with_container(flist) as cont:
-        update_healthcheck(service, node.healthcheck.run(cont, 'openfiledescriptors'))
+    update_healthcheck(service, node.healthcheck.check_ofd())
     update_healthcheck(service, node.healthcheck.calc_cpu_mem())
     # call log rotator
     update_healthcheck(service, node.healthcheck.rotate_logs())
     service.saveAll()
 
 
-def update_healthcheck(service, messages):
+def update_healthcheck(service, healthchecks):
     import time
+
     interval = service.model.actionGet('monitor').period
-    for message in messages:
-        health = None
+    new_healthchecks = list()
+    for health_check in healthchecks:
         for health in service.model.data.healthchecks:
-            if health.id == message['id']:
-                health.name = message.get('name', "")
-                health.resource = message.get('resource', "")
-                health.status = message.get('status', "")
-                health.message = message.get('message', "")
-                health.lasttime =time.time()
+            # If this healthcheck already exists, update its attributes
+            if health.id == health_check['id']:
+                health.name = health_check.get('name', '')
+                health.resource = health_check.get('resource', '')
+                health.messages = health_check.get('messages', [])
+                health.category = health_check.get('category', '')
+                health.lasttime = time.time()
                 health.interval = interval
+                health.stacktrace = health_check.get('stacktrace', '')
                 break
         else:
-            healthchecks = []
-            for item in service.model.data.healthchecks:
-                healthcheck = {}
-                healthcheck['id'] = item.id
-                healthcheck['name'] = item.name
-                healthcheck['resource'] = item.resource
-                healthcheck['status'] = item.status
-                healthcheck['message'] = item.message
-                healthcheck['lasttime'] = time.time()
-                healthcheck['interval'] = interval
-                healthchecks.append(healthcheck)
-            healthchecks.append(message)
-            service.model.data.healthchecks = healthchecks
+            # healthcheck doesn't exist in the current list, add it to the list of new
+            new_healthchecks.append(health_check)
+
+    old_healthchecks = service.model.data.to_dict().get('healthchecks', [])
+    old_healthchecks.extend(new_healthchecks)
+    service.model.data.healthchecks = old_healthchecks
 
 
 def reboot(job):
