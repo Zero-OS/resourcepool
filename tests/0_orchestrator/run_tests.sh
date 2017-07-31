66c655d0e172416d5537192a23da2515cd0b5f2c
islamTaha12 <tahai@codescalers.com>
2017-07-31 12:00:03 +0200

    install packet machines before the env

--- tests/0_orchestrator/run_tests.sh
+++ tests/0_orchestrator/run_tests.sh
@@ -7,12 +7,10 @@ if [ "$TRAVIS_EVENT_TYPE" == "cron" ] || [ "$TRAVIS_EVENT_TYPE" == "api" ]
     then
       pip3 install -r tests/0_orchestrator/test_suite/requirements.txt
       pip3 install git+https://github.com/gigforks/packet-python.git
-      bash tests/0_orchestrator/install_env.sh master $ZT_NET_ID $ZT_TOKEN
       cd tests/0_orchestrator/; python3 orch_packet_machines.py create $PACKET_TOKEN $ZT_NET_ID $ITSYOUONLINE_ORG $TRAVIS_BRANCH
+      bash tests/0_orchestrator/install_env.sh master $ZT_NET_ID $ZT_TOKEN
    elif [ "$point" == "run" ]
     then
-      echo "sleeping 333"
-      sleep 333
       echo "Running tests .."
       cd tests/0_orchestrator/test_suite
       export PYTHONPATH='./'
