8ed7188be113e4c72acb38390c6be9f86c77a90d
abdulrahmantkhalifa <abdulrahman.t.khalifa@gmail.com>
2017-08-08 13:01:03 +0200

    incorrect parameter passed (#843)

--- pyclient/zeroos/orchestrator/sal/healthchecks/ssh_cleanup.py
+++ pyclient/zeroos/orchestrator/sal/healthchecks/ssh_cleanup.py
@@ -24,7 +24,7 @@ class SSHCleanup(HealthCheckRun):
                 if job_dict['actionName'] == 'processChange' and job_dict['actorName'] == 'vm':
                     if job_dict['state'] == 'running':
                         continue
-                    vm = self.service.aysrepo.serviceGet(name=job_dict['serviceName'], role=job_dict['actorName'])
+                    vm = self.service.aysrepo.serviceGet(instance=job_dict['serviceName'], role=job_dict['actorName'])
                     finished.append("ssh.config_%s" % vm.name)
             for proc in self.node.client.process.list():
                 for partial in finished:
