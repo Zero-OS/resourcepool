d0b006609a59bdf3ed542c80d074ae25bb1eaeb4
Jo De Boeck <deboeck.jo@gmail.com>
2017-08-09 14:16:06 +0200

    Fix python install of orchestrator (#851)
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- pyclient/setup.py
+++ pyclient/setup.py
@@ -21,7 +21,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        'zeroos.orchestrator.sal.templates': ['*', 'dashboards/*']
+        'zeroos.orchestrator.sal.templates': ['*.conf', 'dhcp', 'dashboards/*']
     },
     namespace_packages=['zeroos'],
     install_requires=['python-dateutil', 'Jinja2'],
