be90288b07bcb9dadde6d094018b6ab7ab2645a9
islamTaha12 <tahai@codescalers.com>
2017-08-10 16:27:32 +0200

    fix export

--- tests/0_orchestrator/install_env.sh
+++ tests/0_orchestrator/install_env.sh
@@ -92,7 +92,7 @@ ssh -tA root@localhost -p 2222 "export GIGDIR=~/gig; curl -sL https://raw.github
 echo "Enabling JWT..."
 cd tests/0_orchestrator/
 scp -P 2222 enable_jwt.sh root@localhost:
-ssh -tA root@localhost -p 2222 "export ITSYOUONLINE_CL_ID=$ITSYOUONLINE_CL_ID; export ITSYOUONLINE_CL_SECRET=$ITSYOUONLINE_CL_SECRET; export ITSYOUONLINE_ORG=$ITSYOUONLINE_ORG; export $CORE_0_BRANCH source enable_jwt.sh"
+ssh -tA root@localhost -p 2222 "export ITSYOUONLINE_CL_ID=$ITSYOUONLINE_CL_ID; export ITSYOUONLINE_CL_SECRET=$ITSYOUONLINE_CL_SECRET; export ITSYOUONLINE_ORG=$ITSYOUONLINE_ORG; export CORE0_BRANCH=$CORE_0_BRANCH; source enable_jwt.sh"
 
 # get orch-server ip
 orch_ip=$(ssh -At root@localhost -p 2222 "ip addr show zt0 | grep 'inet'")
