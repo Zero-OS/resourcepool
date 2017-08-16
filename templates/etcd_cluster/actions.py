fa6fc48a80e95fc8b93f517afc8b393944ab03f1
Abdulrahman <tareka@codescalers.com>
2017-08-16 12:05:26 +0200

    added handler for container eof for etcd container fails
    this relates to issue #847

--- templates/etcd_cluster/actions.py
+++ templates/etcd_cluster/actions.py
@@ -105,3 +105,29 @@ def get_baseports(job, node, baseport, nrports):
             if len(freeports) >= nrports:
                 return freeports, tcpservices
         baseport += 1
+
+def watchdog_handler(job):
+    from zeroos.orchestrator.sal.Container import Container
+    from zeroos.orchestrator.sal.Node import Node
+    from zeroos.orchestrator.configuration import get_jwt_token
+
+    service = job.service
+    etcds = service.producers.get('etcd', [])
+    
+    # check on etcd container since the watch dog will handle the actual service 
+    for etcd in etcds:
+        container = etcd.parent
+        node = container.parent
+        token = get_jwt_token(job.service.aysrepo)
+
+        container_client = Container.from_ays(container, password=token)
+        if not container_client.id:
+            node_client = Node.from_ays(node, password=token)
+            if not node_client.client.ping():
+                raise j.exceptions.RunTimeError("node %s with Etcd %s is down" %(node.name, etcd.name))
+
+            j.tools.async.wrappers.sync(container.executeAction('start', context=job.context))
+            j.tools.async.wrappers.sync(etcd.executeAction('start', context=job.context))
+            
+
+
