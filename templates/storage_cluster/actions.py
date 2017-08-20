6eddf4f1919e26f73fe085c41312aa34ade82f3a
Khaled K. Badr <khaledkbadr@outlook.com>
2017-08-20 12:29:48 +0200

    fixes
    Signed-off-by: Khaled K. Badr <khaledkbadr@outlook.com>

--- templates/storage_cluster/actions.py
+++ templates/storage_cluster/actions.py
@@ -326,8 +326,8 @@ def monitor(job):
     old_orphans = set()
     for orphan_service in old_orphans_services:
         # Delete orphan vdisk if operator didn't act for 7 days
-        oprhan_time = (int(time.time()) - orphan_service.model.data.timestamp) / (7 * 3600 * 24)
-        if oprhan_time >= 7:
+        orphan_time = (int(time.time()) - orphan_service.model.data.timestamp) / (3600 * 24)
+        if orphan_time >= 7:
             j.tools.async.wrappers.sync(orphan_service.executeAction('delete', context=job.context))
             j.tools.async.wrappers.sync(orphan_service.delete())
             continue
