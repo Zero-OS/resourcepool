ad66d730e7f6908680df5392a0c97e49fc2e2d3b
abdulrahmantkhalifa <abdulrahman.t.khalifa@gmail.com>
2017-08-28 10:57:06 +0200

    better handling for ssh cleanup health check (#913)

--- pyclient/zeroos/orchestrator/sal/healthchecks/ssh_cleanup.py
+++ pyclient/zeroos/orchestrator/sal/healthchecks/ssh_cleanup.py
@@ -25,13 +25,14 @@ class SSHCleanup(HealthCheckRun):
                     if job_dict['state'] == 'running':
                         continue
                     vm = self.service.aysrepo.serviceGet(instance=job_dict['serviceName'], role=job_dict['actorName'])
-                    tcp_services = vm.producers.get('tcp')
+                    tcp_services = vm.producers.get('tcp', [])
                     for tcp_service in tcp_services:
                         if 'migrationtcp' not in tcp_service.name:
                             continue
                         j.tools.async.wrappers.sync(tcp_service.executeAction("drop"), context=self.job.context)
                         j.tools.async.wrappers.sync(tcp_service.delete())
                     finished.append("ssh.config_%s" % vm.name)
+
             for proc in self.node.client.process.list():
                 for partial in finished:
                     if partial not in proc['cmdline']:
