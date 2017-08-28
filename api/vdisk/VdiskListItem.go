42ce6e6df4043efdce1ee66a6a6bb524a4b83249
Khaled K. Badr <khaledkbadr@outlook.com>
2017-08-28 13:09:22 +0200

    Add missing fields in api.raml
    Signed-off-by: Khaled K. Badr <khaledkbadr@outlook.com>

--- api/vdisk/VdiskListItem.go
+++ api/vdisk/VdiskListItem.go
@@ -5,10 +5,12 @@ import (
 )
 
 type VdiskListItem struct {
-	ID             string                   `yaml:"id" json:"id" validate:"nonzero"`
-	Status         EnumVdiskStatus          `yaml:"status" json:"status,omitempty"`
-	Storagecluster string                   `yaml:"storageCluster" json:"storageCluster" validate:"nonzero"`
-	Vdisktype      EnumVdiskCreateVdisktype `yaml:"type" json:"type" validate:"nonzero"`
+	ID                   string                   `yaml:"id" json:"id" validate:"nonzero"`
+	Status               EnumVdiskStatus          `yaml:"status" json:"status,omitempty"`
+	BlockStoragecluster  string                   `yaml:"blockStoragecluster" json:"blockStoragecluster" validate:"nonzero"`
+	ObjectStoragecluster string                   `yaml:"objectStoragecluster" json:"objectStoragecluster" validate:"nonzero"`
+	BackupStoragecluster string                   `yaml:"backupStoragecluster" json:"backupStoragecluster" validate:"nonzero"`
+	Vdisktype            EnumVdiskCreateVdisktype `yaml:"type" json:"type" validate:"nonzero"`
 }
 
 func (s VdiskListItem) Validate() error {
