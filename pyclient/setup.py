560f9696f5d5979e6e052bcf2f1316a4900a1127
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-08-31 12:12:27 +0200

    publish new version python client

--- pyclient/setup.py
+++ pyclient/setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='0-orchestrator',
-    version='1.1.0-alpha-7',
+    version='1.1.0-alpha-7_1',
     description='Zero-OS Orchestrator',
     long_description=long_description,
     url='https://github.com/g8os/grid',
