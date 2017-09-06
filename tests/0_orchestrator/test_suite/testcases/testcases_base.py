846ea317132a2bdd973b9dd9711d32cdf5be0264
Ahmed El-Sayed <ahmed.m.elsayed-93@gmail.com>
2017-09-06 08:32:48 +0200

    fix jwt issue

--- tests/0_orchestrator/test_suite/testcases/testcases_base.py
+++ tests/0_orchestrator/test_suite/testcases/testcases_base.py
@@ -22,11 +22,8 @@ class TestcasesBase(TestCase):
         self.vdisks_api = self.orchasterator_driver.vdisks_api
         self.vms_api = self.orchasterator_driver.vms_api
         self.zerotiers_api = self.orchasterator_driver.zerotiers_api
-
         self.zerotier_token = self.orchasterator_driver.zerotier_token
-        self.jwt = self.orchasterator_driver.JWT
         self.nodes_info = self.orchasterator_driver.nodes_info
-
         self.session = requests.Session()
         self.session.headers['Authorization'] = 'Bearer {}'.format(self.zerotier_token)
 
@@ -45,6 +42,7 @@ class TestcasesBase(TestCase):
         self.nodeipList = [x['ip'] for x in self.nodes_info if x['id'] == self.nodeid]
         self.assertNotEqual(self.nodeipList, [])
         self.nodeip = self.nodeipList[0]
+        self.jwt = self.orchasterator_driver.get_jwt()
         self.core0_client = Client(self.nodeip, password=self.jwt)
 
     def randomMAC(self):
