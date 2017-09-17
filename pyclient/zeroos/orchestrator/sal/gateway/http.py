24df85a62a3ab2f12279d9c3056f4cbe2c14cdf0
Muhamad Azmy <muhamad.azmy@codescalers.com>
2017-09-17 10:28:04 +0200

    Fix monitoring for http service

--- pyclient/zeroos/orchestrator/sal/gateway/http.py
+++ pyclient/zeroos/orchestrator/sal/gateway/http.py
@@ -4,18 +4,19 @@ import time
 
 
 class HTTPServer:
-    def __init__(self, container, type, httpproxies):
+    def __init__(self, container, service, httpproxies):
         self.container = container
-        self.type = type
+        self.service = service
         self.httpproxies = httpproxies
 
     def id(self):
-        return 'caddy-{}.{}'.format(self.type, self.container.name)
+        return 'http.{}'.format(self.service.name)
 
     def apply_rules(self):
         # caddy
-        caddyconfig = templates.render('caddy.conf', type=self.type, httpproxies=self.httpproxies).strip()
-        conf = '/etc/caddy-{}.conf'.format(self.type)
+        type = self.service.model.data.type
+        caddyconfig = templates.render('caddy.conf', type=type, httpproxies=self.httpproxies).strip()
+        conf = '/etc/caddy-{}.conf'.format(type)
         self.container.upload_content(conf, caddyconfig)
         self.container.client.job.kill(self.id(), int(signal.SIGUSR1))
         if caddyconfig == '':
@@ -34,7 +35,7 @@ class HTTPServer:
             self.container.client.job.list(self.id())
         except:
             return False
-        portnr = 80 if self.type == 'http' else 443
+        portnr = 80 if self.service.model.data.type == 'http' else 443
         for port in self.container.client.info.port():
             if port['network'] .startswith('tcp') and port['port'] == portnr:
                 return True
