3c1780228193bcf575e96eee8eeb0c6527b6e4c0
Geert Audenaert <geert@mobicage.com>
2017-09-22 20:54:33 +0200

    Refactorted DeleteHTTPProxies

--- api/handlers/node/nodes_api_CreateHTTPProxies.go
+++ api/handlers/node/nodes_api_CreateHTTPProxies.go
@@ -8,8 +8,9 @@ import (
 
 	"github.com/gorilla/mux"
 
+	"github.com/zero-os/0-orchestrator/api/ays"
+	"github.com/zero-os/0-orchestrator/api/handlers"
 	"github.com/zero-os/0-orchestrator/api/httperror"
-	"github.com/zero-os/0-orchestrator/api/tools"
 )
 
 // CreateHTTPProxies is the handler for POST /nodes/{nodeid}/gws/{gwname}/httpproxies
@@ -34,12 +35,18 @@ func (api *NodeAPI) CreateHTTPProxies(w http.ResponseWriter, r *http.Request) {
 	gateway := vars["gwname"]
 	nodeID := vars["nodeid"]
 
-	queryParams := map[string]interface{}{
-		"parent": fmt.Sprintf("node.zero-os!%s", nodeID),
-	}
+	// queryParams := map[string]interface{}{
+	// 	"parent": fmt.Sprintf("node.zero-os!%s", nodeID),
+	// }
 
-	service, res, err := aysClient.Ays.GetServiceByName(gateway, "gateway", api.AysRepo, nil, queryParams)
-	if !tools.HandleAYSResponse(err, res, w, "Getting gateway service") {
+	// service, res, err := aysClient.Ays.GetServiceByName(gateway, "gateway", api.AysRepo, nil, queryParams)
+	// if !tools.HandleAYSResponse(err, res, w, "Getting gateway service") {
+	// 	return
+	// }
+	parent := fmt.Sprintf("node.zero-os!%s", nodeID)
+	service, err := api.client.GetService("gateway", gateway, parent, nil)
+	if err != nil {
+		handlers.HandleError(w, err)
 		return
 	}
 
@@ -67,18 +74,30 @@ func (api *NodeAPI) CreateHTTPProxies(w http.ResponseWriter, r *http.Request) {
 
 	data.Httpproxies = append(data.Httpproxies, reqBody)
 
-	obj := make(map[string]interface{})
-	obj[fmt.Sprintf("gateway__%s", gateway)] = data
+	// obj := make(map[string]interface{})
+	// obj[fmt.Sprintf("gateway__%s", gateway)] = data
 
-	run, err := aysClient.ExecuteBlueprint(api.AysRepo, "gateway", gateway, "update", obj)
-	errMessage := fmt.Sprintf("error executing blueprint for gateway %s", gateway)
-	if !tools.HandleExecuteBlueprintResponse(err, w, errMessage) {
+	serviceName := fmt.Sprintf("gateway__%s", gateway)
+	blueprint := ays.Blueprint{
+		serviceName: data,
+	}
+
+	// run, err := aysClient.ExecuteBlueprint(api.AysRepo, "gateway", gateway, "update", obj)
+	// errMessage := fmt.Sprintf("error executing blueprint for gateway %s", gateway)
+	// if !tools.HandleExecuteBlueprintResponse(err, w, errMessage) {
+	// 	return
+	// }
+
+	// if _, errr := aysClient.WaitOnRun(w, api.AysRepo, run.Key); errr != nil {
+	// 	return
+	// }
+
+	blueprintName := ays.BlueprintName("gateway", gateway, "update")
+	if _, err := api.client.CreateExecRun(blueprintName, blueprint, true); err != nil {
+		handlers.HandleError(w, err)
 		return
 	}
 
-	if _, errr := aysClient.WaitOnRun(w, api.AysRepo, run.Key); errr != nil {
-		return
-	}
 	w.Header().Set("Location", fmt.Sprintf("/nodes/%s/gws/%s/httpproxies/%v", nodeID, gateway, reqBody.Host))
 	w.WriteHeader(http.StatusCreated)
 
