c2a5211b6df4663e4c577556e7a9c6031f22a0b4
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-09-22 21:22:06 +0200

    update vendor folder

--- api/ays/run.go
+++ api/ays/run.go
@@ -7,13 +7,8 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/pborman/uuid"
-	client "github.com/zero-os/0-orchestrator/api/ays/ays-client"
 	"github.com/zero-os/0-orchestrator/api/ays/callback"
 )
-
-// Run represents a Run in AYS. IT contains all the steps AYS will do
-// to execute some actions on the services
-type Run = client.AYSRun
 
 // CreateRun creates a run
 // if simulate is false, the run is not started just after creation, else the run is started directly
@@ -84,18 +79,4 @@ func (c *Client) ExecuteRun(runID string, wait bool) (*Run, error) {
 	}
 
 	return c.GetRun(runID)
-}
-
-// GetRun retreive a run from AYS server
-func (c *Client) GetRun(runID string) (*Run, error) {
-	run, resp, err := c.AYS().GetRun(runID, c.repo, nil, nil)
-	if err != nil {
-		return nil, newError(resp, err)
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, newError(resp, fmt.Errorf("get run return status %v", resp.Status))
-	}
-
-	return run, nil
 }
