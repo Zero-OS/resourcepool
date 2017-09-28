dd0845dbfacdb048e94a2cbc9296dc2a5d6ed287
Abdulrahman <tareka@codescalers.com>
2017-09-28 11:28:30 +0200

    cleaner validation, vdiskstor parent for vdisk
    - service validation moved to validate method on struct
    - vdiskstor became parent of vdisk stor for better readabilty and to
      avoid situation of empty list error if does not exists

--- api/vdiskstorage/CreateNewVdiskStorage.go
+++ api/vdiskstorage/CreateNewVdiskStorage.go
@@ -20,12 +20,12 @@ func (api *VdiskstorageAPI) CreateNewVdiskStorage(w http.ResponseWriter, r *http
 	}
 
 	// validate request
-	if err := reqBody.Validate(); err != nil {
+	if err := reqBody.Validate(aysClient, api); err != nil {
 		tools.WriteError(w, http.StatusBadRequest, err, "")
 		return
 	}
 
-	// validate vdiskstorage name
+	// check if service exists
 	exists, err := aysClient.ServiceExists("vdiskstorage", reqBody.ID, api.AysRepo)
 
 	if err != nil {
@@ -35,47 +35,8 @@ func (api *VdiskstorageAPI) CreateNewVdiskStorage(w http.ResponseWriter, r *http
 
 	if exists {
 		err = fmt.Errorf("vdiskstorage with name %s already exists", reqBody.ID)
-		tools.WriteError(w, http.StatusBadRequest, err, "")
+		tools.WriteError(w, http.StatusConflict, err, "")
 		return
-	}
-
-	// validate block cluster name
-	exists, err = aysClient.ServiceExists("storage_cluster", reqBody.BlockCluster, api.AysRepo)
-	if err != nil {
-		tools.WriteError(w, http.StatusInternalServerError, err, "Error checking storage_cluster service exists")
-		return
-	}
-	if !exists {
-		err = fmt.Errorf("storage_cluster with name %s does not exists", reqBody.BlockCluster)
-		tools.WriteError(w, http.StatusBadRequest, err, "")
-		return
-	}
-
-	// validate object cluster name
-	if reqBody.ObjectCluster != "" {
-		exists, err = aysClient.ServiceExists("storage_cluster", reqBody.ObjectCluster, api.AysRepo)
-		if err != nil {
-			tools.WriteError(w, http.StatusInternalServerError, err, "Error checking storage_cluster service exists")
-			return
-		}
-		if !exists {
-			err = fmt.Errorf("storage_cluster with name %s does not exists", reqBody.ObjectCluster)
-			tools.WriteError(w, http.StatusBadRequest, err, "")
-			return
-		}
-		if reqBody.SlaveCluster != "" {
-			// validate slave cluster name
-			exists, err = aysClient.ServiceExists("storage_cluster", reqBody.SlaveCluster, api.AysRepo)
-			if err != nil {
-				tools.WriteError(w, http.StatusInternalServerError, err, "Error checking storage_cluster service exists")
-				return
-			}
-			if !exists {
-				err = fmt.Errorf("storage_cluster with name %s does not exists", reqBody.SlaveCluster)
-				tools.WriteError(w, http.StatusBadRequest, err, "")
-				return
-			}
-		}
 	}
 
 	obj := make(map[string]interface{})
