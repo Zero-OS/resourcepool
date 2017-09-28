460904bdab505eeead96ab2dd9e8c104807710a0
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-09-28 17:22:07 +0200

    implement GetImage and ListImages

--- api/vdiskstorage/vdiskstorage_api_GetImage.go
+++ api/vdiskstorage/vdiskstorage_api_GetImage.go
@@ -3,11 +3,29 @@ package vdiskstorage
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/zero-os/0-orchestrator/api/tools"
 )
 
 // GetImage is the handler for GET /vdiskstorage/{vdiskstorageid}/images/{imageid}
 // Get detail about a vdisk image
-func (api VdiskstorageAPI) GetImage(w http.ResponseWriter, r *http.Request) {
-	var respBody Image
-	json.NewEncoder(w).Encode(&respBody)
+func (api *VdiskstorageAPI) GetImage(w http.ResponseWriter, r *http.Request) {
+	aysClient := tools.GetAysConnection(r, api)
+	imageID := mux.Vars(r)["imageid"]
+	var image Image
+
+	imageService, resp, err := aysClient.Ays.GetServiceByName(imageID, "image", api.AysRepo, nil, nil)
+	if !tools.HandleAYSResponse(err, resp, w, "fail to get image service") {
+		return
+	}
+
+	if err := json.Unmarshal(imageService.Data, &image); err != nil {
+		tools.WriteError(w, http.StatusInternalServerError, err, "fail to get image service")
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&image)
 }
--- api/vdiskstorage/vdiskstorage_api_ListImages.go
+++ api/vdiskstorage/vdiskstorage_api_ListImages.go
@@ -3,11 +3,61 @@ package vdiskstorage
 import (
 	"encoding/json"
 	"net/http"
+	"sync"
+
+	ays "github.com/zero-os/0-orchestrator/api/ays-client"
+	"github.com/zero-os/0-orchestrator/api/tools"
 )
 
 // ListImages is the handler for GET /vdiskstorage/{vdiskstorageid}/images
 // List all vdisk images installed in this VdiskStroage
-func (api VdiskstorageAPI) ListImages(w http.ResponseWriter, r *http.Request) {
-	var respBody []Image
-	json.NewEncoder(w).Encode(&respBody)
+func (api *VdiskstorageAPI) ListImages(w http.ResponseWriter, r *http.Request) {
+	aysClient := tools.GetAysConnection(r, api)
+	// vdiskstorageID := mux.Vars(r)["vdiskstorageid"]
+
+	services, res, err := aysClient.Ays.ListServicesByRole("vdisk_image", api.AysRepo, nil, nil)
+	if !tools.HandleAYSResponse(err, res, w, "listing vdisk_images") {
+		return
+	}
+
+	var (
+		images = make([]Image, len(services))
+		errs   = make([]error, len(services))
+		wg     = sync.WaitGroup{}
+	)
+	wg.Add(len(services))
+	for i, serviceData := range services {
+		go func(i int, serviceData *ays.ServiceData) {
+			defer wg.Done()
+
+			service, _, err := aysClient.Ays.GetServiceByName(serviceData.Name, serviceData.Role, api.AysRepo, nil, nil)
+			if err != nil {
+				errs[i] = err
+				return
+			}
+
+			var image Image
+			if err := json.Unmarshal(service.Data, &image); err != nil {
+				errs[i] = err
+				return
+			}
+
+			image.Id = serviceData.Name
+			images[i] = image
+
+		}(i, &serviceData)
+	}
+
+	wg.Wait()
+
+	// TODO: concatenate all  errors into one
+	for _, err := range errs {
+		if err != nil {
+			tools.WriteError(w, http.StatusInternalServerError, err, "Error getting list of images")
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&images)
 }
