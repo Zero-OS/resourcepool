37289f4994d8f1a52fc89fe6b7f3427b5bc588fe
[Dinaamagdy] <[magdyd@greenitglobe.com]>
2017-10-22 15:16:29 +0200

    change to proftpd

--- tests/0_orchestrator/install_ftp_server.sh
+++ tests/0_orchestrator/install_ftp_server.sh
@@ -1,53 +1,37 @@
 #!/usr/bin bash
 
-update_ftp_configuration(){
-sudo cat >> /etc/vsftpd.conf << EOL
-listen=NO
-listen_ipv6=YES
-allow_writeable_chroot=YES
-anonymous_enable=YES
-local_enable=NO
-write_enable=YES
-anon_upload_enable=YES
-anon_other_write_enable=YES
-chroot_local_user=yes
-anon_root=/var/ftp/
-anon_mkdir_write_enable=YES
-dirmessage_enable=YES
-use_localtime=YES
-no_anon_password=YES
-xferlog_enable=YES
-connect_from_port_20=YES
+update_proftp_configuration(){
+sudo cat >> /etc/proftpd/proftpd.conf << EOL
+ <Anonymous ~ftp>
+   User                         ftp
+   Group                                nogroup
+   # We want clients to be able to login with "anonymous" as well as "ftp"
+   UserAlias                    anonymous ftp
+   # Cosmetic changes, all files belongs to ftp user
+   DirFakeUser  on ftp
+   DirFakeGroup on ftp
+
+   RequireValidShell            off
+
+ </Anonymous>
 EOL
                           }
 upload_file_in_ftp(){
-ftp -v -n  $1 <<END_SCRIPT
-user ftp
+lftp  $1 <<END_SCRIPT
+mkdir pub
 cd pub
-mkdir file
-cd file
 put $2
 quit
 END_SCRIPT
 exit 0
 ~
 }
-
 sudo apt-get update
-echo "[#] Installing local ftp server ..."
-
-sudo apt-get install vsftpd -y
-sudo apt-get install ftp
-sudo mv /etc/vsftpd.conf /etc/vsftpd.conf.orig
-
-echo "[#] Update ftp_server  configuration. "
-update_ftp_configuration
-sudo systemctl restart vsftpd
-
-echo "[#] Make sure that ftp_server is working by upload file . "
-sudo mkdir -p /var/ftp/pub
-sudo chmod 777 /var/ftp/pub
-cd /var/ftp/pub
+sudo apt-get install proftpd-basic -y
+sudo apt-get install lftp
+sudo cp /etc/proftpd/proftpd.conf /etc/proftpd/proftpd.conf.orig
+update_proftp_configuration
+sudo systemctl restart proftpd
 File_name=test.text
 touch ${File_name}
 ftpserver_ip=$(ifconfig bond0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
