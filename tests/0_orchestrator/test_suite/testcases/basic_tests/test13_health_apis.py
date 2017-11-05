b5658292fff86bec5b9e849c0325ea9b2020d1fe
Ahmed El-Sayed <ahmed.m.elsayed-93@gmail.com>
2017-11-05 13:22:07 +0200

    fix

--- tests/0_orchestrator/test_suite/testcases/basic_tests/test13_health_apis.py
+++ tests/0_orchestrator/test_suite/testcases/basic_tests/test13_health_apis.py
@@ -5,6 +5,7 @@ class TestNodeHealthcheckAPI(TestcasesBase):
 
     def setUp(self):
         super().setUp()
+        self.core0_client.client.timeout = 20
         self.lg.info(' [*] Get healthcheck of random node ')
         response = self.healthcheck_api.get_node_health(nodeid=self.nodeid)
         self.assertEqual(response.status_code, 200)
@@ -45,100 +46,91 @@ class TestNodeHealthcheckAPI(TestcasesBase):
         **Test Scenario:**
 
         #. Get random node (N0).
-        #. Choose random free disk (FD0) from node (N0) disks.
-        #. Create btrfs filesystem and Mount disk (FD0).
-        #. Write file (F1) with size equal to (91 %) of disk (FD0) space.
-        #. Get node (N0) healthchecks, disk (FD0) usage status should be warning.
-        #. Remove file (F1).
-        #. Write file (F2) with size equal to (95 %) of disk (FD0) space.
-        #. Get node (N0) healthchecks, disk (FD0) usage status should be error.
-        #. Remove file (F2).
+        #. Choose random free device (DE0) from node (N0) devices.
+        #. Create storagepool (SP0) and attach device (DE0) to it, should succeed.
+        #. Create filesystem (FS0), should succeed.
+        #. Get node (N0) healthchecks, device (DE0) usage status should be (OK).
+        #. Write file (F0) with size equal to (91 %) of device (DE0) space.
+        #. Get node (N0) healthchecks, device (DE0) usage status should be (WARNING).
+        #. Remove file (F0).
+        #. Write file (F1) with size equal to (95 %) of device (DE0) space.
+        #. Get node (N0) healthchecks, device (DE0) usage status should be (ERROR).
+        #. Delete storagepool (SP0), should succeed.
         """
+        def get_disk_usage(target_disk):
+            if target_disk['type'] == 'nvme':
+                targe_disk_name = '{}p1_usage'.format(target_disk['name'])
+            else:
+                targe_disk_name = '{}1_usage'.format(target_disk['name'])
 
-        def get_disk_usage(diskname):
             response = self.healthcheck_api.get_node_health(nodeid=self.nodeid)
             self.assertEqual(response.status_code, 200)
 
             healthchecks = response.json()['healthchecks']
             disk_usage = [x for x in healthchecks if x['id'] == 'disk-usage'][0]
-            target_disk_usage = [x for x in disk_usage['messages'] if x['id'] == '{}_usage'.format(diskname)]
+            target_disk_usage = [x for x in disk_usage['messages'] if x['id'] == targe_disk_name]
             self.assertNotEqual(target_disk_usage, [])
             return target_disk_usage[0]
 
-        def write_file_to_disk(filesize, path):
-            filename = self.random_string()
-            cmd = 'cd {path}; fallocate -l {filesize}G {filename}'.format(
-                path=path,
-                filename=filename,
-                filesize=filesize
-            )
-            response = self.core0_client.bash(cmd)
-            self.assertEqual(response.state, 'SUCCESS')
-            return filename
-
-        def remove_file_from_disk(path, filename):
-            cmd = 'rm -f {}/{}'.format(path, filename)
-            response = self.core0_client.bash(cmd)
-            self.assertEqual(response.state, 'SUCCESS')
-
-
-        node_free_disks = self.core0_client.getFreeDisks()
-        
-        if not node_free_disks:
-            self.skipTest('No free disks to preform tests')
-        
-        self.lg.info('Choose random free disk (FD0) from node (N0) disks ')
+        node_free_disks = self.core0_client.getFreeDisks() 
         target_disk = random.choice(node_free_disks)
-        mountpoint = '/mnt/{}'.format(self.random_string())
 
-        self.lg.info('Create btrfs filesystem and Mount disk (FD0)')
-        cmd = 'mkdir {mountpoint}; mkfs.btrfs {target_disk}; mount {target_disk} {mountpoint}'.format(
-            target_disk=target_disk['name'],
-            mountpoint=mountpoint
+        self.lg.info('Choose random free device (DE0) from node (N0) devices')
+        response, storagepool = self.storagepools_api.post_storagepools(self.nodeid, free_devices=[target_disk['name']])
+        self.assertEqual(response.status_code, 201)
+
+        self.lg.info('Create filesystem (FS0), should succeed')
+        response, filesystem = self.storagepools_api.post_storagepools_storagepoolname_filesystems(
+            self.nodeid, storagepool['name'], quota=0, readOnly=False
+        )
+        self.assertEqual(response.status_code, 201)
+
+        time.sleep(35)
+
+        self.lg.info('Get node (N0) healthchecks, device (DE0) usage status should be (OK)')
+        disk_usage = get_disk_usage(target_disk)
+        self.assertEqual(disk_usage['status'], 'OK')
+
+        path = '/mnt/storagepools/{}/filesystems/{}'.format(storagepool['name'], filesystem['name'])
+                
+        self.lg.info('Write file (F0) with size equal to (91 %) of device (DE0) space')
+        filename = self.random_string()
+        filesize = int(0.91 * target_disk['size'])
+        cmd = 'cd {path}; fallocate -l {filesize}G {filename}'.format(
+            path=path,
+            filename=filename,
+            filesize=filesize
         )
         response = self.core0_client.bash(cmd)
         self.assertEqual(response.state, 'SUCCESS')
 
-        self.lg.info('Write file (F1) with size equal to (91 %) of disk (FD0) space')
-        filesize = int(0.91 * target_disk['size'])
-        filename = write_file_to_disk(filesize=filesize, path=mountpoint)
+        time.sleep(35)
 
-        time.sleep(30)
-
-        self.lg.info('Get node (N0) healthchecks, disk (FD0) usage status should be warning')
-        disk_usage = get_disk_usage(target_disk['name'])
+        self.lg.info('Get node (N0) healthchecks, device (DE0) usage status should be (WARNING)')
+        disk_usage = get_disk_usage(target_disk)
         self.assertEqual(disk_usage['status'], 'WARNING')
 
-        self.lg.info('Remove file (F1)')
-        remove_file_from_disk(mountpoint, filename)
+        self.lg.info('Remove file (F0)')
+        cmd = 'rm -f {}/*'.format(path)
+        self.core0_client.bash(cmd)
 
-        self.lg.info('Write file (F2) with size equal to (95 %) of disk (FD0) space')
+        self.lg.info('Write file (F1) with size equal to (95 %) of device (DE0) space')
+        filename = self.random_string()
         filesize = int(0.95 * target_disk['size'])
-        filename = write_file_to_disk(filesize=filesize, path=mountpoint)
+        cmd = 'cd {path}; fallocate -l {filesize}G {filename}'.format(
+            path=path,
+            filename=filename,
+            filesize=filesize
+        )
+        response = self.core0_client.bash(cmd)
+        self.assertEqual(response.state, 'SUCCESS')
 
-        time.sleep(30)
+        time.sleep(35)
 
-        self.lg.info('Get node (N0) healthchecks, disk (FD0) usage status should be error')
-        disk_usage = get_disk_usage(target_disk['name'])
+        self.lg.info('Get node (N0) healthchecks, device (DE0) usage status should be (ERROR)')
+        disk_usage = get_disk_usage(target_disk)
         self.assertEqual(disk_usage['status'], 'ERROR')
 
-        self.lg.info('Remove file (F2)')
-        remove_file_from_disk(mountpoint, filename)
-        self.core0_client.client.bash('umount {}'.format(mountpoint))
-        self.core0_client.client.bash('dd if=/dev/zero bs=1M count=500 of={}'.format(target_disk['name']))
-
-
-
-
-
- 
-
-
-
-
-        
-
-
-
-
-
+        self.lg.info('Delete storagepool (SP0), should succeed')
+        response = self.storagepools_api.delete_storagepools_storagepoolname(self.nodeid, storagepool['name'])
+        self.assertEqual(response.status_code, 204)
