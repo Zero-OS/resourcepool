ec5b1ef2206c54349b23788232650684fe9a9c21
Abdelrahman Hussein <husseina@greenitglobe.com>
2017-12-19 15:27:23 +0100

    harden the check for zerotie ip

--- scripts/install-orchestrator.sh
+++ scripts/install-orchestrator.sh
@@ -16,6 +16,41 @@ error_handler() {
 
 trap 'error_handler $LINENO' ERR
 
+# Test an IP address for validity:
+# Usage:
+#      valid_ip IP_ADDRESS
+#      if [[ $? -eq 0 ]]; then echo good; else echo bad; fi
+#   OR
+#      if valid_ip IP_ADDRESS; then echo good; else echo bad; fi
+#
+function valid_ip()
+{
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
+# Function to retrieve the zeroteir ip address and return it
+set_zerotierip(){
+    ZEROTIERIP=`zerotier-cli  listnetworks  | grep ${ZEROTIERNWID} | awk '{print $NF}' | awk -F / '{print $1}'`
+    if !valid_ip $ZEROTIERIP; then
+        ZEROTIERIP=`zerotier-cli  listnetworks  | grep ${ZEROTIERNWID} | awk '{print $NF}' | awk -F , '{print $1}' | awk -F / '{print $1}'`
+    fi
+    if !valid_ip $ZEROTIERIP; then
+      ZEROTIERIP=""
+    fi
+}
+
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 if ! grep -q "^${LANG}" /etc/locale.gen; then
@@ -213,7 +248,7 @@ go build -o /usr/local/bin/orchestratorapiserver >> ${logfile} 2>&1
 
 echo "[+] Starting orchestrator api server"
 orchinit="/etc/my_init.d/11_orchestrator.sh"
-ZEROTIERIP=`zerotier-cli  listnetworks  | grep ${ZEROTIERNWID} | awk '{print $NF}' | awk -F , '{print $1}' | awk -F / '{print $1}'`
+set_zerotierip()
 
 if [ "$ZEROTIERIP" == "" ]; then
     echo "zerotier doesn't have an ip. make sure you have authorize this docker in your netowrk"
